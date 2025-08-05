a922adde3a6332f307a8081b56b264aa9e81babc
Christian Flores <cefg7382@gmail.com>
2025-08-05 09:56:06 -0500

    Agrego archivo netlify-hamburgermenu.jsx y navbar.jsxm

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,21 @@ import Carrusel from './components/Carrusel'
 import ContactForm from './components/ContactForm'
 import WhatsAppFloatingButton from './components/WhatsAppFloatingButton'
 import HamburgerMenu from './components/HamburgerMenu';
+import Navbar from './components/Navbar';
   
 function App() {
   return (
-    <>   
-      <HamburgerMenu />    
-      <Header />
-      <FeatureSection />
-      <Carrusel />
-      <ContactForm />
-      <WhatsAppFloatingButton />
-      
+    <>       
+      <Navbar />
+      {/* Espaciado para que no se oculte debajo del navbar fijo */}
+      <div className="pt-20"> 
+        <Header />  
+        <HamburgerMenu />            
+        <FeatureSection />
+        <Carrusel />
+        <ContactForm />
+        <WhatsAppFloatingButton />
+      </div>
     </>
   )
 }
