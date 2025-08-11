9995518e70da202309614f2eb1d2ebf9fdda4479
Christian Flores <cefg7382@gmail.com>
2025-08-10 19:10:34 -0500

    adicionaste imagen

--- src/pages/Mantenimiento.jsx
+++ src/pages/Mantenimiento.jsx
@@ -13,7 +13,7 @@ const Mantenimiento = () => {
       }}
     >
       {/* Capa oscura para contraste */}
-      <div className="absolute inset-0 bg-black bg-opacity-20"></div>
+      <div className="absolute inset-0 bg-black bg-opacity-5"></div>
 
       <div className="relative max-w-6xl mx-auto text-center text-white">
         <h2 className="text-4xl font-bold mb-4">
