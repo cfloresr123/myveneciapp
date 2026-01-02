c9802551607b35c261b1e983bcdea6718b7cf47e
Christian Flores <cefg7382@gmail.com>
2026-01-02 15:45:48 -0500

    remove duplicate file EliminarCuentas.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,6 +16,7 @@ const Footer = () => {
             <li><a href="/Visitas" className="hover:text-yellow-400">Visitas</a></li>
             <li><a href="/Contacto" className="hover:text-yellow-400">Contáctanos</a></li>
             <li><a href="/Privacy" className="hover:text-yellow-400">Privacidad</a></li>
+            <li><a href="/EliminarCuenta" className="hover:text-yellow-400">Elimina Cuenta</a></li>
 
           </ul>
         </div>
